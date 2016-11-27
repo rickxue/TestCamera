6d37695bbfaf7f7da68001e8f263feecc6553e19
rick.xue <rick.xue@zeusis.com>
2016-11-27 12:57:54 +0800

    test
    Signed-off-by: rick.xue <rick.xue@zeusis.com>

--- app/src/main/java/com/zeusis/testcamera/MainActivity.java
+++ app/src/main/java/com/zeusis/testcamera/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
     tv.setText(stringFromJNI());
+
+    
     }
 
     /**
